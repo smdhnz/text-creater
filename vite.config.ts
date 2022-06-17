a5d9c02dc112eaed83fee9bbfd7f80c70443c15f
smdhnz <strikerfmy1208@gmail.com>
2022-06-17 19:59:49 +0900

    vite.config.tsにbaseを設定

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
+  base: '/text-creater/',
   plugins: [
     react(),
     tsconfigPaths(),
